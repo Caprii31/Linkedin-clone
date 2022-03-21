ddf70786663f2cd1e75b5bbc7920037f4e6b655f
unknown <ahmedkamal.su31@gmail.com>
2022-03-21 02:03:37 +0200

    post

--- src/components/home/posts/SinglePost.js
+++ src/components/home/posts/SinglePost.js
@@ -2,6 +2,12 @@ import { Avatar } from '@mui/material'
 import React from 'react'
 import PublicIcon from '@mui/icons-material/Public';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import PostOption from './PostOption';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ChatIcon from '@mui/icons-material/Chat';
+import ReplyIcon from '@mui/icons-material/Reply';
+import SendIcon from '@mui/icons-material/Send';
+
 
 function SinglePost() {
   return (
@@ -25,7 +31,12 @@ function SinglePost() {
         <div className="main__post">
             <p>hey my name is ahmed kamal am graduated from faculty of law i started front learning front-end from about year ago i started with cs50 it taught me basics of com science and then i took fromt-end nanodegree from udacity </p>
         </div>
-        {/* likes comment */}
+        <div className="social__options">
+            <PostOption Icon = {ThumbUpIcon} title='like' color='#5E5E5E'/>
+            <PostOption Icon = {ChatIcon} title='comment' color='#5E5E5E'/>
+            <PostOption Icon = {ReplyIcon} title='share' color='#5E5E5E'/>
+            <PostOption Icon = {SendIcon} title='send' color='#5E5E5E'/>
+        </div>
     </div>
   )
 }
